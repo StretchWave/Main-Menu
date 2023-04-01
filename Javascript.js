8cf79dfe90a36edd7ef41751f1ee336ab08045eb
StretchWave <78847401+StretchWave@users.noreply.github.com>
2023-04-01 14:16:18 +0530

    Main HTML

---
+++ Javascript.js
@@ -0,0 +1,11 @@
+function NavOpen()
+{
+    document.body.classList.toggle("nav-open");
+    document.getElementById("MenuBarOpen").style.display = "none";
+}
+function NavClose()
+{
+    document.body.classList.toggle("nav-open");
+    document.getElementById("MenuBarOpen").style.display = "block";
+}
+
